1d04259c0ee7731f38af03d1b7d880f9dd09acf7
zjy1084200013 <1084200013@qq.com>
2020-10-29 17:19:36 +0800

    项目经理发了一个空项目，大家做一下

---
+++ src/test/java/com/usian/UsianGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsianGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
